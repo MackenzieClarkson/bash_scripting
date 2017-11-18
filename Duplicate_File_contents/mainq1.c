fc8ca853c8f89df1f467297878f3078c870a10c8
Mac <mackenzie.clarkson@uoit.net>
2017-11-18 12:29:16 -0500

    fsa

--- Duplicate_File_contents/mainq1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "duplicate.c"
-int main(int argc, char *argv[]){
-    duplicate(argc,argv);
-
-}
--- Duplicate_File_contents/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "duplicate.c"
-int main(){
-    int argc = 3; 
-    char *argv[] = {"\0", "src.txt", "dupe.txt"}; //hard coded testing inputs
-
-    duplicate(argc, argv); //pass arguments to duplicate function
-}
